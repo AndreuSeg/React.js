d9dccfce4e1b10f4dbe2efbd42b15c2a9a1a17b0
Andreu Seguí <andreuseguisegura@gmail.com>
2022-10-09 17:43:50 +0200

    Sesion 2 react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/pure/greeting';
+import GreetingF from './components/pure/greetingF';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola Mundo
-        </p>
+        {/* {Componente propio Greeting.jsx} */}
+        {/* <Greeting></Greeting> */}
+        <GreetingF name="Andreu"></GreetingF>
       </header>
     </div>
   );
